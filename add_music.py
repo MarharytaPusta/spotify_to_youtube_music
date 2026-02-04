eb14f48f196a24ab5d9b676561b4ce970147f244
MarharytaPusta <marharitapustaa@gmail.com>
2026-02-04 23:55:43 +0200

    add songs to the playlist

--- add_music.py
+++ add_music.py
@@ -12,17 +12,18 @@ if __name__ == "__main__":
     driver.get("https://music.youtube.com/")
     time.sleep(2)
 
-    # signin = driver.find_element(By.CSS_SELECTOR, ".sign-in-link")
-    # driver.execute_script("arguments[0].click()", signin)
-    # input("Коли успішно увійдете і сторінка завантажиться, натисніть ENTER")
+    signin = driver.find_element(By.CSS_SELECTOR, ".sign-in-link")
+    driver.execute_script("arguments[0].click()", signin)
+    input("Коли успішно увійдете і сторінка завантажиться, натисніть ENTER")
+
     # elem = driver.find_element(By.CSS_SELECTOR, '#sections #buttons [class="style-scope ytmusic-guide-section-renderer"] button[class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading yt-spec-button-shape-next--enable-backdrop-filter-experiment"]')
     # driver.execute_script("arguments[0].click()", elem)
     # time.sleep(2)
-    #
-    # with open("songs.txt", "r", encoding="UTF-8") as f:
-    #     for i, line in enumerate(f):
-    #         if i == 0:
-    #             playlist_name = line.strip()
+    with open("songs.txt", "r", encoding="UTF-8") as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                playlist_name = line.strip()
+    print(f".......{playlist_name}...........")
     # elem = driver.find_element(By.CSS_SELECTOR, '#title-input input')
     # elem.click()
     # elem.send_keys(playlist_name)
@@ -38,21 +39,46 @@ if __name__ == "__main__":
             if i == 0:
                 continue
             song = line.strip()
+            # search
             elem = driver.find_element(By.CSS_SELECTOR, '[class="search-box style-scope ytmusic-search-box"] input#input')
-            elem.click()
+            driver.execute_script("arguments[0].click()", elem)
             elem.send_keys(song)
             elem.send_keys(Keys.ENTER)
             time.sleep(1)
-            elem = driver.find_element(By.ID, 'clear-button')
-            elem.click()
-            elem = driver.find_element(By.CSS_SELECTOR, '[class="main-action-container style-scope ytmusic-card-shelf-renderer"] #button-shape')
-            elem.click()
+            # 3 colon
+            elem = driver.find_element(By.CSS_SELECTOR, 'ytmusic-shelf-renderer ytmusic-responsive-list-item-renderer .menu button')
+            driver.execute_script("arguments[0].click()", elem)
             time.sleep(1)
+            # add to playlist button
             elem = driver.find_element(By.XPATH, '/html/body/ytmusic-app/ytmusic-popup-container/tp-yt-iron-dropdown/div/ytmusic-menu-popup-renderer/tp-yt-paper-listbox/ytmusic-menu-navigation-item-renderer[2]/a/yt-formatted-string')
             driver.execute_script("arguments[0].click()", elem)
+            time.sleep(1)
+            # плейлісти
+            playlists = driver.find_elements(By.CSS_SELECTOR, '[class="scrollable-content scroller style-scope ytmusic-add-to-playlist-renderer"] #playlists ytmusic-playlist-add-to-option-renderer button #title')
 
-
-
+            target_playlist = driver.find_element(By.XPATH, f"//ytmusic-playlist-add-to-option-renderer//yt-formatted-string[@title='{playlist_name}' or text()='{playlist_name}']")
+            # Клікаємо на знайдений плейліст
+            driver.execute_script("arguments[0].click()", target_playlist)
+            print("Ура! Пісню додано.")
+            # for i in range(len(playlists)):
+            #     playlists[i] = playlists[i].text
+            # print(f"names: \n{playlists}")
+            # if playlist_name not in playlists:
+            #     print(f"Wops, somthing went wrong: {playlist_name} not in")
+            #     print(f"names: {playlists}")
+            # else:
+            #     print("start")
+            #     child_number_of_playlist = playlists.index(playlist_name) + 1
+            #     child_number_of_playlist = int(child_number_of_playlist)
+            #     # concrete playlist
+            #     elem = driver.find_element(By.CSS_SELECTOR, f'[class="scrollable-content scroller style-scope ytmusic-add-to-playlist-renderer"] #playlists:nth-child({child_number_of_playlist}) button[class="style-scope ytmusic-playlist-add-to-option-renderer"]')
+            #     driver.execute_script("arguments[0].click()", elem)
+            #     print("uhu, 1 more song")
+            # print("dcds")
+            # clear button
+            elem = driver.find_element(By.ID, 'clear-button')
+            driver.execute_script("arguments[0].click()", elem)
+            time.sleep(0.5)
 """
 #id
 .class
