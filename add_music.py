42e4d4f125f62e2b29b3f7aaead914f4b5c5b1d1
MarharytaPusta <marharitapustaa@gmail.com>
2026-02-15 19:01:37 +0200

    refactoring

--- add_music.py
+++ add_music.py
@@ -6,94 +6,73 @@ import undetected_chromedriver as uc
 import time
 import reader
 
+class AddMusic:
+    def __init__(self):
+        self.driver = uc.Chrome(version_main=144)
+        self.playlist_name = ""
+
+    def login(self):
+        self.driver.get("https://music.youtube.com/")
+        time.sleep(2)
+
+        signin = self.driver.find_element(By.CSS_SELECTOR, ".sign-in-link")
+        self.driver.execute_script("arguments[0].click()", signin)
+        input("When you successfully log in and the page loads, press ENTER")
+
+    def get_playlist_name(self):
+        with open("songs.txt", "r", encoding="UTF-8") as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    self.playlist_name = line.strip()
+
+    def add_songs(self):
+        with open("songs.txt", "r", encoding="UTF-8") as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    continue
+                song = line.strip()
+                # search
+                elem = self.driver.find_element(By.CSS_SELECTOR, '[class="search-box style-scope ytmusic-search-box"] input#input')
+                self.driver.execute_script("arguments[0].click()", elem)
+                elem.send_keys(song)
+                elem.send_keys(Keys.ENTER)
+                time.sleep(1)
+                # 3 colon
+                elem = self.driver.find_element(By.CSS_SELECTOR, 'ytmusic-shelf-renderer ytmusic-responsive-list-item-renderer .menu button')
+                self.driver.execute_script("arguments[0].click()", elem)
+                time.sleep(1)
+                # add to playlist button
+                try:
+                    elem = self.driver.find_element(By.XPATH, '/html/body/ytmusic-app/ytmusic-popup-container/tp-yt-iron-dropdown/div/ytmusic-menu-popup-renderer/tp-yt-paper-listbox/ytmusic-menu-navigation-item-renderer[2]/a/yt-formatted-string')
+                    self.driver.execute_script("arguments[0].click()", elem)
+                    time.sleep(1)
+                    # playlist
+                    target_playlist = self.driver.find_element(By.XPATH, f"//ytmusic-playlist-add-to-option-renderer//yt-formatted-string[@title='{self.playlist_name}' or text()='{self.playlist_name}']")
+                    self.driver.execute_script("arguments[0].click()", target_playlist)
+                    elem = self.driver.find_element(By.ID, 'clear-button')
+                    self.driver.execute_script("arguments[0].click()", elem)
+                    time.sleep(0.5)
+                except:
+                    print("We can't find the playlist")
+                    return
+
+
+def add_music_to_playlist():
+    bot = AddMusic()
+    bot.login()
+    bot.get_playlist_name()
+    try:
+        bot.add_songs()
+    except:
+        print("Something went wrong. Maybe you haven't log in")
+
 if __name__ == "__main__":
-    driver = uc.Chrome(version_main=144)
-
-    driver.get("https://music.youtube.com/")
-    time.sleep(2)
-
-    signin = driver.find_element(By.CSS_SELECTOR, ".sign-in-link")
-    driver.execute_script("arguments[0].click()", signin)
-    input("Коли успішно увійдете і сторінка завантажиться, натисніть ENTER")
-
-    # elem = driver.find_element(By.CSS_SELECTOR, '#sections #buttons [class="style-scope ytmusic-guide-section-renderer"] button[class="yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading yt-spec-button-shape-next--enable-backdrop-filter-experiment"]')
-    # driver.execute_script("arguments[0].click()", elem)
-    # time.sleep(2)
-    with open("songs.txt", "r", encoding="UTF-8") as f:
-        for i, line in enumerate(f):
-            if i == 0:
-                playlist_name = line.strip()
-    print(f".......{playlist_name}...........")
-    # elem = driver.find_element(By.CSS_SELECTOR, '#title-input input')
-    # elem.click()
-    # elem.send_keys(playlist_name)
-    # elem = driver.find_element(By.CSS_SELECTOR, '[class="actions style-scope ytmusic-playlist-form"] [class="style-scope ytmusic-playlist-form"] button[class="yt-spec-button-shape-next yt-spec-button-shape-next--filled yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--enable-backdrop-filter-experiment"]')
-    # driver.execute_script("arguments[0].click()", elem)
+    print("For the addition to be successful, you must have a single playlist in YouTube Music that is named the same as in Spotify")
+    # is_any_song = True
+    is_any_song = reader.read_songs("data.txt")
+    if (is_any_song == False):
+        print("You have no song in the playlist or you have some problem with file")
+    else:
+        add_music_to_playlist()
 
 
-
-    #finding a music
-    song = ''
-    with open("songs.txt", "r", encoding="UTF-8") as f:
-        for i, line in enumerate(f):
-            if i == 0:
-                continue
-            song = line.strip()
-            # search
-            elem = driver.find_element(By.CSS_SELECTOR, '[class="search-box style-scope ytmusic-search-box"] input#input')
-            driver.execute_script("arguments[0].click()", elem)
-            elem.send_keys(song)
-            elem.send_keys(Keys.ENTER)
-            time.sleep(1)
-            # 3 colon
-            elem = driver.find_element(By.CSS_SELECTOR, 'ytmusic-shelf-renderer ytmusic-responsive-list-item-renderer .menu button')
-            driver.execute_script("arguments[0].click()", elem)
-            time.sleep(1)
-            # add to playlist button
-            elem = driver.find_element(By.XPATH, '/html/body/ytmusic-app/ytmusic-popup-container/tp-yt-iron-dropdown/div/ytmusic-menu-popup-renderer/tp-yt-paper-listbox/ytmusic-menu-navigation-item-renderer[2]/a/yt-formatted-string')
-            driver.execute_script("arguments[0].click()", elem)
-            time.sleep(1)
-            # плейлісти
-            playlists = driver.find_elements(By.CSS_SELECTOR, '[class="scrollable-content scroller style-scope ytmusic-add-to-playlist-renderer"] #playlists ytmusic-playlist-add-to-option-renderer button #title')
-
-            target_playlist = driver.find_element(By.XPATH, f"//ytmusic-playlist-add-to-option-renderer//yt-formatted-string[@title='{playlist_name}' or text()='{playlist_name}']")
-            # Клікаємо на знайдений плейліст
-            driver.execute_script("arguments[0].click()", target_playlist)
-            print("Ура! Пісню додано.")
-            # for i in range(len(playlists)):
-            #     playlists[i] = playlists[i].text
-            # print(f"names: \n{playlists}")
-            # if playlist_name not in playlists:
-            #     print(f"Wops, somthing went wrong: {playlist_name} not in")
-            #     print(f"names: {playlists}")
-            # else:
-            #     print("start")
-            #     child_number_of_playlist = playlists.index(playlist_name) + 1
-            #     child_number_of_playlist = int(child_number_of_playlist)
-            #     # concrete playlist
-            #     elem = driver.find_element(By.CSS_SELECTOR, f'[class="scrollable-content scroller style-scope ytmusic-add-to-playlist-renderer"] #playlists:nth-child({child_number_of_playlist}) button[class="style-scope ytmusic-playlist-add-to-option-renderer"]')
-            #     driver.execute_script("arguments[0].click()", elem)
-            #     print("uhu, 1 more song")
-            # print("dcds")
-            # clear button
-            elem = driver.find_element(By.ID, 'clear-button')
-            driver.execute_script("arguments[0].click()", elem)
-            time.sleep(0.5)
-"""
-#id
-.class
-
-[href='/favourites']"
-[type='submit']
-
-[type='text'][name='email']
-a#offers[href='/offers']
-
-[class^='Navbar_logo_']           щоб задати початок назви класу
-[class$='26S5Y']                  щоб задати кінець назви класу
-[class*='logo_']                  щоб задати довільну частину назви класу
-
-button.buttonКлас +-
-.buttonКлас +-
-[name='button1'] +-
-"""
--- reader.py
+++ reader.py
@@ -1,82 +1,110 @@
-
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException
 
-if __name__ == "__main__":
-    with open("data.txt") as f:
-        for i, line in enumerate(f):
-            if i == 0:
-                playlist = line.strip()
-                print(playlist)
 
-    driver = webdriver.Chrome()
-    driver.get(playlist)
-    time.sleep(5)
+class SporifyWriter:
+    def __init__(self, file_name):
+        self.driver = webdriver.Chrome()
+        self.playlist_url = ""
+        self.songs = set()
+        self.file_name = file_name
 
-    songs = set()
-    previous_count = 0
-    stuck_counter = 0
+    def get_playlist_url(self):
+            with open(self.file_name) as file:
+                for i, line in enumerate(file):
+                    if i == 0:
+                        self.playlist_url = line.strip()
 
-    while True:
-        try:
-            #плейліст
-            elem = driver.find_element(By.CSS_SELECTOR, '[data-testid="playlist-tracklist"]')
-            #пісні всередині
-            rows = elem.find_elements(By.CSS_SELECTOR, '[data-testid="tracklist-row"]')
-        except:
-            time.sleep(1)
-            continue
-        for row in rows:
+    def open_playlist(self):
+        self.driver.get(self.playlist_url)
+        time.sleep(5)
+
+    def read_songs(self):
+        previous_count = 0
+        stuck_counter = 0
+        no_song_case = 0
+        while True:
             try:
-                title = row.find_element(By.CSS_SELECTOR, '[data-testid="internal-track-link"] div').text
-                button = row.find_element(By.CSS_SELECTOR, 'button[aria-label]')
-                aria_text = button.get_attribute("aria-label")
-                index = aria_text.find(title)
-                if index != -1:
-                    aria_text = aria_text[index:]
-                index = len(title) + 1
-                aria_text = aria_text[index:]
-                index = aria_text.find(' ') + 1
-                artist = aria_text[index:]
-
-                full = f"{title} — {artist}"
-                songs.add(full)
-            except Exception:
+                elem = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="playlist-tracklist"]')
+                rows = elem.find_elements(By.CSS_SELECTOR, '[data-testid="tracklist-row"]')
+            except:
+                no_song_case += 1
+                if no_song_case > 7:
+                    return
+                time.sleep(1)
                 continue
-
-        current_count = len(songs)
-        print(current_count)
-        if current_count == previous_count:
-            stuck_counter += 1
-            if stuck_counter >= 8:
-                print("all")
-                break
-        else:
-            stuck_counter = 0
-            previous_count = current_count
-
-        try:
-            fresh_rows = elem.find_elements(By.CSS_SELECTOR, '[data-testid="tracklist-row"]')
-            if fresh_rows:
-                last_row = fresh_rows[-1]
-                driver.execute_script("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", last_row)
+            for row in rows:
+                try:
+                    title = row.find_element(By.CSS_SELECTOR, '[data-testid="internal-track-link"] div').text
+                    button = row.find_element(By.CSS_SELECTOR, 'button[aria-label]')
+                    aria_text = button.get_attribute("aria-label")
+                    index = aria_text.find(title)
+                    if index != -1:
+                        aria_text = aria_text[index:]
+                    index = len(title) + 1
+                    aria_text = aria_text[index:]
+                    index = aria_text.find(' ') + 1
+                    artist = aria_text[index:]
+                    full = f"{title} — {artist}"
+                    self.songs.add(full)
+                except Exception:
+                    continue
+            current_count = len(self.songs)
+            if current_count == previous_count:
+                stuck_counter += 1
+                if stuck_counter >= 8:
+                    break
             else:
-                driver.execute_script("window.scrollBy(0, 500);")
+                stuck_counter = 0
+                previous_count = current_count
+
+            try:
+                fresh_rows = elem.find_elements(By.CSS_SELECTOR, '[data-testid="tracklist-row"]')
+                if fresh_rows:
+                    last_row = fresh_rows[-1]
+                    self.driver.execute_script("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});",
+                                               last_row)
+                else:
+                    self.driver.execute_script("window.scrollBy(0, 500);")
+            except:
+                self.driver.execute_script("window.scrollBy(0, 600);")
+
+            time.sleep(1)
+
+    def save_to_file(self):
+        try:
+            playlist_name = self.driver.find_element(By.CSS_SELECTOR,
+                                                     '.main-view-container__scroll-node-child [data-testid="playlist-page"] [role="grid"][aria-label]')
+            playlist_name = playlist_name.get_attribute("aria-label")
         except:
-            driver.execute_script("window.scrollBy(0, 600);")
+            playlist_name = "unknown playlist"
+        with open("songs.txt", "w", encoding="UTF-8") as f:
+            f.write(f"{playlist_name}\n")
+            for song in self.songs:
+                f.write(f"{song}\n")
 
-        time.sleep(1)
+    def is_any_song(self):
+        if len(self.songs) > 0:
+            return True
+        else:
+            return False
+
+    def end(self):
+        self.driver.quit()
 
 
-    playlist_name = driver.find_element(By.CSS_SELECTOR,
-                                        '.main-view-container__scroll-node-child [data-testid="playlist-page"] [role="grid"][aria-label]')
-    playlist_name = playlist_name.get_attribute("aria-label")
-
-    with open("songs.txt", "w", encoding="UTF-8") as f:
-        f.write(f"{playlist_name}\n")
-        for song in songs:
-            f.write(f"{song}\n")
-
-    driver.quit()
+def read_songs(file_name):
+    reader = SporifyWriter(file_name)
+    try:
+        reader.get_playlist_url()
+    except:
+        print("Something wrong with data file")
+        return False
+    reader.open_playlist()
+    reader.read_songs()
+    reader.save_to_file()
+    reader.end()
+    is_any_song_there = reader.is_any_song()
+    return is_any_song_there
